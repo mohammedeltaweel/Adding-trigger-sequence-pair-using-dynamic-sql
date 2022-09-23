125e79e895382c2fbb52e358d9b84184a654cae9
mohammedeltaweel <mohammedeltaweel40@gmail.com>
2022-09-23 13:17:28 +0200

    Update PL SQL Code.sql

--- PL SQL Code.sql
+++ PL SQL Code.sql
@@ -1,13 +1,17 @@
 SET SERVEROUTPUT ON
 DECLARE
         CURSOR add_seq_cursor IS
-            SELECT cols.table_name, cols.column_name, cols.position, cons.status, cons.owner
-            FROM all_constraints cons, all_cons_columns cols
-            WHERE cons.constraint_type = 'P'  and cols.owner = 'HR'
-            AND cons.constraint_name = cols.constraint_name
-            AND cons.owner = cols.owner
-            AND position=1;
+            SELECT DISTINCT cls.table_name, cls.column_name, cls.position, cns.status, cns.owner,  tbl_cls.data_type
+            FROM all_constraints cns, all_cons_columns cls, user_tab_columns tbl_cls
+            WHERE cns.constraint_type = 'P'  and cls.owner = 'HR'
+            AND cns.table_name = tbl_cls.table_name
+            AND cns.constraint_name = cls.constraint_name
+            AND cns.owner = cls.owner
+            AND position=1
+            AND tbl_cls.data_type = 'NUMBER'
+            AND NULLABLE = 'N';
             
+ 
 v_count number(4);
 BEGIN
     FOR rec IN add_seq_cursor LOOP
@@ -15,12 +19,12 @@ BEGIN
     --CHECK IF THE SEQUENCE ALREADY EXISTS
     SELECT COUNT(*) into v_count
     FROM all_sequences
-    WHERE sequence_owner = 'HR' AND sequence_name = rec.table_name || '_' || rec.position || '_SEQ';
+    WHERE sequence_owner = 'HR' AND sequence_name = rec.table_name  || '_SEQ';
       IF (v_count = 0) THEN
     
-    --CREATING THE SEQUENCE WITH NAME OF TABLE_NAME_POSITION_SEQ
-    EXECUTE IMMEDIATE 'CREATE SEQUENCE ' || rec.table_name || '_' || rec.position ||'_SEQ 
-            START WITH 100 
+    --CREATING THE SEQUENCE WITH NAME OF TABLE_NAME_POSITION_SEQ 
+    EXECUTE IMMEDIATE 'CREATE SEQUENCE ' || rec.table_name || '_SEQ 
+            START WITH 100000 
             INCREMENT BY 5 
             MAXVALUE 9999999999 
             MINVALUE 1 
@@ -28,15 +32,13 @@ BEGIN
             NOORDER' ;
     END IF;
     
-     EXECUTE IMMEDIATE 'CREATE OR REPLACE TRIGGER HR.'|| rec.table_name || '_trg' ||
+    EXECUTE IMMEDIATE  'CREATE OR REPLACE TRIGGER HR.'|| rec.table_name || '_TRG' ||
 ' BEFORE INSERT
 ON HR.' || rec.table_name || ' REFERENCING NEW AS New OLD AS Old
 FOR EACH ROW
 BEGIN
-    :new.' || rec.column_name || ' :=  ' ||rec.table_name || '_' || rec.position ||'_SEQ.nextval;
-END;'  ;
-
-
+    :new.' || rec.column_name || ' :=  ' ||rec.table_name ||'_SEQ.nextval;
+END;' ;
     END LOOP;
 
 END;
